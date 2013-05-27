ea1a2badc3047cc337cd39895912586d763cb552
Drew DeVault <sir@cmpwn.com>
2013-05-27 14:52:12 -0600

    Added NBT serialization

---
+++ fNbt/Serialization/INbtSerializable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using fNbt;
+
+namespace fNbt.Serialization
+{
+    public interface INbtSerializable
+    {
+        NbtCompound Serialize();
+        void Deserialize(NbtCompound value);
+    }
+}
