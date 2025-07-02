dc03c4b27b4d964447da2ff81ec70e7eda22a0e0
ssp47 <ssp474138610@163.com>
2025-07-02 10:54:22 +0800

    ### feat: Enhance NBT Serialization with Source Generation and Attributes
    This pull request introduces significant improvements to NBT (Named Binary Tag) serialization and deserialization, making it as simple and intuitive as System.Text.Json, while also supporting AOT (Ahead-of-Time) compilation for .NET 6/8+ projects.
    
    #### New Classes and Interfaces:
    
    1. `fNbt.Serialization.INbtSerializableType`: Interface for serializable objects.
    2. `fNbt.Serialization.INbtDeserializableType`: Interface for deserializable objects.
    3. `fNbt.Serialization.NbtDeserializationHelper`: Helper class for deserialization.
    4. `fNbt.Serialization.NbtSerializationHelper`: Helper class for serialization.
    5. `fNbt.Serialization.NbtSerializableType`: Attribute for automatic serialization implementation.
    6. `fNbt.Serialization.NbtPropertyName`: Attribute for specifying NBT tag names.
    7. `fNbt.Serialization.NotNbtProperty`: Attribute for excluding properties from serialization.
    
    #### Detailed Changes:
    
    1. **Interface Implementation:**
       - Introduced `INbtSerializableType` and `INbtDeserializableType` interfaces.
       - These interfaces define `SerializeToNbt` and `DeserializeFromNbt` methods respectively.
       - Implementing these interfaces allows for easy composition of serializable types.
    
    2. **Helper Classes:**
       - `NbtDeserializationHelper` and `NbtSerializationHelper` provide overloads for basic types (int, long, arrays).
       - These classes are intentionally non-static to allow users to add extension methods for simple or sealed types.
       - This design facilitates seamless integration with source generation.
    
    3. **Attributes for Source Generation:**
       - `NbtSerializableType`: Automatically implements NBT serialization and deserialization.
       - `NbtPropertyName`: Specifies the NBT tag name (similar to `JsonPropertyName`).
       - `NotNbtProperty`: Excludes a property from serialization/deserialization.
    
    4. **Serialization Process:**
       - Objects can be easily serialized to NBT format and exported to files.
       - Remember to set the `Name` property of the serialized NBT tag before using it as the `RootTag` of an `NbtFile`.
    
    #### Example Usage:
    
    ```csharp
    NbtTag serializeToNbt = personalCareer.SerializeToNbt();
    serializeToNbt.Name = "Mike";
    Console.WriteLine(serializeToNbt);
    new NbtFile{RootTag = (NbtCompound)serializeToNbt}.SaveToFile("TestFiles/TestSerialize.personalCareer.nbt", NbtCompression.None);

---
+++ GenSerializer/AutoSerializer.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using fNbt;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace GenSerializer {
+    [Generator]
+    public class AutoSerializer: IIncrementalGenerator{
+        public void Initialize(IncrementalGeneratorInitializationContext context) {
+            IncrementalValuesProvider<INamedTypeSymbol> incrementalValuesProvider = context.SyntaxProvider.CreateSyntaxProvider(
+                predicate: Utils.FindNbtSerializableType,
+                transform: Utils.GetNbtSerializableType);
+            
+            context.RegisterSourceOutput(incrementalValuesProvider,Excute);
+            
+        }
+
+
+        private void Excute(SourceProductionContext context, INamedTypeSymbol classAuto) {
+            //Gets properties that do not contain the NotNbtProperty Attribute and then foreach
+            IEnumerable<IPropertySymbol> propertySymbols = classAuto.GetMembers()
+                                                                    .Where(v=>v is IPropertySymbol propertySymbol
+                                                                                           && !propertySymbol.GetAttributes()
+                                                                                               .Any(p=>p.ToString().Contains("NotNbtProperty")))
+                                                                    .Cast<IPropertySymbol>().ToArray();
+
+            string genSource = $$"""
+                                namespace {{classAuto.ContainingNamespace.ToDisplayString()}}{
+                                {{classAuto.DeclaredAccessibility.ToString().ToLower()}} partial class {{classAuto.Name}} : fNbt.Serialization.INbtDeserializableType,fNbt.Serialization.INbtSerializableType
+                                    {
+                                    {{GetSerialization(propertySymbols)}}
+                                    {{GetDeserialization(propertySymbols)}}
+                                    }
+                                }
+                                """;
+            context.AddSource($"{classAuto.ContainingNamespace.ToDisplayString()}.{classAuto.Name}Nbt.g.cs",genSource);
+            return;
+        }
+
+        private static string GetNbtPropertyName(IPropertySymbol item)
+        {
+            // 获取 NbtPropertyName 特性
+            AttributeData? attributeData = item.GetAttributes()
+                                               .FirstOrDefault(v => v.AttributeClass?.Name.Contains("NbtPropertyName") == true);
+            // 如果没有找到特性，则使用 item.Name 作为默认值
+            if (attributeData == null)
+            {
+                return item.Name;
+            }
+            // 获取特性的命名参数
+            ImmutableArray<KeyValuePair<string, TypedConstant>> keyValuePairs = attributeData.NamedArguments;
+            // 如果没有命名参数，则使用 item.Name 作为默认值
+            if (keyValuePairs.IsDefaultOrEmpty) //注意这里要判断是否为default or empty
+            {
+                return item.Name;
+            }
+            // 查找名为 "Name" 的键值对
+            KeyValuePair<string, TypedConstant> nameValuePair = keyValuePairs.FirstOrDefault(v => v.Key == "Name");
+            // 如果找到了 "Name" 键值对，并且值不为空，则返回该值
+            if (!String.IsNullOrEmpty(nameValuePair.Key) && nameValuePair.Value.Value is string propertyName)
+            {
+                return propertyName;
+            }
+            // 否则，返回 item.Name 作为默认值
+            return item.Name;
+        }
+         
+        private string GetSerialization(IEnumerable<IPropertySymbol> propertySymbols) {
+            StringBuilder sb = new("		public NbtTag SerializeToNbt(){\n");
+            sb.AppendLine("var helper=new fNbt.Serialization.NbtSerializationHelper();");
+            sb.AppendLine("\t\t\tvar result = new fNbt.NbtCompound();");
+            sb.AppendLine("\t\t\tfNbt.NbtTag obj;");
+            foreach (IPropertySymbol item in propertySymbols) {
+                //Okay, here's the translation and suggested names
+
+                string propertyName = GetNbtPropertyName(item);
+                if (item.Type.AllInterfaces.Any(v=>v.Name=="INbtSerializableType")) {
+                    sb.AppendLine($"\t\t\tobj = {item.Name}.SerializeToNbt();");
+                    sb.AppendLine($"\t\t\tobj.Name = \"{propertyName}\";");
+                    sb.AppendLine($"\t\t\tresult.Add(obj)");
+                    continue;
+                }
+                sb.AppendLine($"\t\t\tobj = helper.SerializeToNbt({item.Name});");
+                sb.AppendLine($"\t\t\tobj.Name = \"{propertyName}\";");
+                sb.AppendLine($"\t\t\tresult.Add(obj);");
+            }
+            sb.AppendLine("\t\t\treturn result;");
+            sb.AppendLine("		}");
+            return sb.ToString();
+        }
+
+
+        private string GetDeserialization(IEnumerable<IPropertySymbol> propertySymbols) {
+            StringBuilder sb = new("		public void DeserializeFromNbt(NbtTag tag){\n");
+            sb.AppendLine("\t\t\tif(tag is not NbtCompound nbtTag) throw new System.ArgumentException(\"tag was not a NbtCompound\");");
+            sb.AppendLine("var helper =new fNbt.Serialization.NbtDeserializationHelper();");
+            foreach (IPropertySymbol item in propertySymbols) {
+                string propertyName = GetNbtPropertyName(item);
+                
+                if (item.Type.AllInterfaces.Any(v=>v.Name=="INbtDeserializableType")) {
+                    sb.AppendLine($"\t\t\t{item.Name} = new();");
+                    sb.AppendLine($"\t\t\t{item.Name}.DeserializeFromNbt(nbtTag[\"{propertyName}\"]);");
+                    //sb.AppendLine($"\t\t\tresult.Add({item.Name}.SerializeToNbt())");
+                    continue;
+                }
+                //sb.AppendLine($"\t\t\tresult.Add(fNbt.Serialization.NbtSerializationHelper.SerializeToNbt({item.Name}));");
+                sb.AppendLine($"\t\t\tvar {item.Name}Temp = {item.Name};");
+                sb.AppendLine($"\t\t\thelper.DeserializeFromNbt(nbtTag[\"{propertyName}\"],out {item.Name}Temp);");
+                sb.AppendLine($"\t\t\t{item.Name} = {item.Name}Temp;");
+            }
+            //sb.AppendLine("\t\t\treturn result;");
+            sb.AppendLine("		}");
+            return sb.ToString();
+        }
+    }
+
+
+    public static class Utils {
+
+        #region Dialog
+
+        public static DiagnosticDescriptor GetOrSetMethonIsNull =
+            new("NBT001", "Can't Find Set or Get", "NbtSerialize Property must have get and set , but '{0}' don't",
+                "Generator", DiagnosticSeverity.Error, true);
+
+
+        public static void ReportGetOrSetMethonIsNullERROR(this SourceProductionContext context, IPropertySymbol property) {
+            context.ReportDiagnostic(Diagnostic.Create(GetOrSetMethonIsNull, property.Locations[0], property.Name));
+        }
+
+        #endregion
+        /// <summary>
+        /// Find Class which has NbtSerializableType(Attribute) 
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="arg2"></param>
+        /// <returns></returns>
+        public static bool FindNbtSerializableType(SyntaxNode node, CancellationToken arg2) {
+            if (node is not ClassDeclarationSyntax cNode|| cNode.AttributeLists.Count==0) return false;
+            return cNode.AttributeLists.Any(attList => attList.Attributes.Any(
+                                           att => att.Name.ToString().Contains("NbtSerializableType")));
+        }
+
+        /// <summary>
+        /// To SemanticModel/Symbol
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="arg2"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static INamedTypeSymbol GetNbtSerializableType(GeneratorSyntaxContext context, CancellationToken arg2) {
+            return context.SemanticModel.GetDeclaredSymbol((ClassDeclarationSyntax)context.Node) as INamedTypeSymbol ?? throw new ArgumentException($"{context.Node.ToFullString()} is not a supported type");
+        }
+    }
+}
---
+++ fNbt/Serialization/NbtSerializationHelper.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace fNbt.Serialization {
+    /// <summary>
+    /// Provides <c>serialization</c> and <c>deserialization</c> support for primitive types.
+    /// <para>The reason is as follows:</para>
+    /// <para>In my design, <c>serialization</c> and <c>deserialization</c> will have a strong binding with <c>source generators</c>.</para>
+    /// <para>When the <c>source generator</c> automatically generates <c></c> and <c></c> functions,</para>
+    /// <para>if there is a relatively simple data type that is not built-in, there is no need to encapsulate the data type at this time.Instead, adding an <c>internal extension method</c> can solve the problem.</para>
+    /// <para>In this way, the <c>source generator</c> will work normally with minimal effort, and provide more editability while being portable.</para>
+    /// <example>
+    /// <code>
+    ///      //The source generator will automatically serialize this type
+    ///      static internal NbtTag SerializeToNbt(this IEnumerable&lt;double&gt; obj) {
+    ///         //TOD Serialize IEnumerable&lt;double&gt; then Return
+    ///      }
+    /// </code>
+    /// </example>
+    /// </summary>
+    public class NbtSerializationHelper {
+        #region Serialization support for primitive data types
+        /// <summary>
+        /// Provide serialization/deserialization support for int32
+        /// </summary>
+        /// <returns></returns>
+        public static NbtTag SerializeToNbt(int obj) {
+            return new NbtInt(obj);
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for int64
+        /// </summary>
+        /// <returns></returns>
+        public static NbtTag SerializeToNbt(long obj) {
+            return new NbtLong(obj);
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for int16
+        /// </summary>
+        /// <returns></returns>
+        public static NbtTag SerializeToNbt(short obj) {
+            return new NbtShort(obj);
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for uint8
+        /// </summary>
+        /// <returns></returns>
+        public static NbtTag SerializeToNbt(byte obj) {
+            return new NbtByte(obj);
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for float32
+        /// </summary>
+        /// <returns></returns>
+        public static NbtTag SerializeToNbt(float obj) {
+            return new NbtFloat(obj);
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for float64(double)
+        /// </summary>
+        /// <returns></returns>
+        public static NbtTag SerializeToNbt(double obj) {
+            return new NbtDouble(obj);
+        }
+        #endregion
+        
+        #region NBT's built-in array-like data types (including string)
+        /// <summary>
+        /// Provide serialization/deserialization support for string
+        /// </summary>
+        /// <returns></returns>
+        public static NbtTag SerializeToNbt(string obj) {
+            return new NbtString(obj);
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for IEnumerable&lt;int(int32)&gt;> to NbtIntArray
+        /// </summary>
+        /// <returns></returns>
+        public static NbtTag SerializeToNbt(IEnumerable<int> obj) {
+            return new NbtIntArray(obj.ToArray());
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for IEnumerable&lt;long(int64)&gt;> to NbtLongArray
+        /// </summary>
+        /// <returns></returns>
+        public static NbtTag SerializeToNbt(IEnumerable<long> obj) {
+            return new NbtLongArray(obj.ToArray());
+        }
+
+        #endregion
+        
+        #region Speical(to NbtList but only INbtSerializableType or NbtTag)
+        /// <summary>
+        /// Provide serialization/deserialization support for IEnumerable&lt;NbtTag&gt;> to NbtList
+        /// </summary>
+        /// <returns></returns>
+        public static NbtTag SerializeToNbt(IEnumerable<NbtTag> obj){
+            return new NbtList(obj);
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for IEnumerable&lt;INbtSerializableType&gt;> to NbtList of NbtCompound(or other,but most of the time are NbtCompound)
+        /// </summary>
+        /// <returns></returns>
+        public static NbtTag SerializeToNbt<TSerialization>(IEnumerable<TSerialization> obj) where TSerialization: INbtSerializableType{
+            return new NbtList(obj.Select(t=>t.SerializeToNbt()));
+        }
+        
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Provides <c>serialization</c> and <c>deserialization</c> support for primitive types
+    /// <seealso cref="NbtSerializationHelper"></seealso>
+    /// </summary>
+    public class NbtDeserializationHelper {
+        
+        #region Deserialization support for primitive data types
+        /// <summary>
+        /// Provide serialization/deserialization support for int32
+        /// </summary>
+        public static void DeserializeFromNbt(NbtTag obj,out int refValue) {
+            if (obj is not NbtInt) {
+                throw new ArgumentException("value of NbtTag is not Int32");
+            }
+            refValue = obj.IntValue;
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for int64
+        /// </summary>
+        public static void DeserializeFromNbt(NbtTag obj,out long refValue) {
+            if (obj is not NbtLong) {
+                throw new ArgumentException("value of NbtTag is not Int64");
+            }
+            refValue = obj.LongValue;
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for int16
+        /// </summary>
+        public static void DeserializeFromNbt(NbtTag obj,out short refValue) {
+            if (obj is not NbtShort) {
+                throw new ArgumentException("value of NbtTag is not Int16");
+            }
+            refValue = obj.ShortValue;
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for uint8(byte)
+        /// </summary>
+        public static void DeserializeFromNbt(NbtTag obj,out byte refValue) {
+            if (obj is not NbtByte) {
+                throw new ArgumentException("value of NbtTag is not UInt8");
+            }
+            refValue = obj.ByteValue;
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for uint8(byte)
+        /// </summary>
+        public static void DeserializeFromNbt(NbtTag obj,out float refValue) {
+            if (obj is not NbtFloat) {
+                throw new ArgumentException("value of NbtTag is not float32");
+            }
+            refValue = obj.FloatValue;
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for float64(double)
+        /// </summary>
+        public static void DeserializeFromNbt(NbtTag obj,out double refValue) {
+            if (obj is not NbtFloat) {
+                throw new ArgumentException("value of NbtTag is not double(float64)");
+            }
+            refValue = obj.DoubleValue;
+        }
+        #endregion
+        
+        #region NBT's built-in array-like data types (including string)
+        /// <summary>
+        /// Provide serialization/deserialization support for string
+        /// </summary>
+        public static void DeserializeFromNbt(NbtTag obj,out string refValue) {
+            if (obj is not NbtString) {
+                throw new ArgumentException("value of NbtTag is not String");
+            }
+            refValue = obj.StringValue;
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for IEnumerable&lt;int(int32)&gt;> from NbtIntArray
+        /// </summary>
+        public static void DeserializeFromNbt(NbtTag obj,out IEnumerable<int> refValue) {
+            if (obj is not NbtIntArray) {
+                throw new ArgumentException("value of NbtTag is not NbtIntArray");
+            }
+            refValue = obj.IntArrayValue;
+        }
+        /// None
+        public static void DeserializeFromNbt(NbtTag obj,out int[] refValue) {
+            if (obj is not NbtIntArray) {
+                throw new ArgumentException("value of NbtTag is not NbtIntArray");
+            }
+            refValue = obj.IntArrayValue;
+        }
+        /// None
+        public static void DeserializeFromNbt(NbtTag obj,out List<int> refValue) {
+            if (obj is not NbtIntArray) {
+                throw new ArgumentException("value of NbtTag is not NbtIntArray");
+            }
+            refValue = obj.IntArrayValue.ToList();
+        }
+        /// <summary>
+        /// Provide serialization/deserialization support for IEnumerable&lt;int(int32)&gt;> from NbtIntArray
+        /// </summary>
+        public static void DeserializeFromNbt(NbtTag obj,out IEnumerable<long> refValue) {
+            if (obj is not NbtLongArray) {
+                throw new ArgumentException("value of NbtTag is not NbtLongArray");
+            }
+            refValue = obj.LongArrayValue;
+        }
+        /// None
+        public static void DeserializeFromNbt(NbtTag obj,out List<long> refValue) {
+            if (obj is not NbtLongArray) {
+                throw new ArgumentException("value of NbtTag is not NbtLongArray");
+            }
+            refValue = obj.LongArrayValue.ToList();
+        }
+        /// None
+        public static void DeserializeFromNbt(NbtTag obj,out long[] refValue) {
+            if (obj is not NbtLongArray) {
+                throw new ArgumentException("value of NbtTag is not NbtLongArray");
+            }
+            refValue = obj.LongArrayValue;
+        }
+        #endregion
+        
+        #region Speical(to NbtList but only INbtDeserializableType or NbtTag)
+        /// <summary>
+        /// Provide serialization/deserialization support for List of INbtDeserializable
+        /// </summary>
+        public static void DeserializeFromNbt<T>(NbtTag obj,out IEnumerable<T> refValue)where T: INbtDeserializableType,new(){
+            if (obj is not NbtList list) {
+                throw new ArgumentException("value of NbtTag is not List");
+            }
+            
+            refValue = list.ToArray().Select(v => {
+                var a = new T();a.DeserializeFromNbt(v); return a;});
+        }
+        
+        
+
+        #endregion
+
+
+
+    }
+}
