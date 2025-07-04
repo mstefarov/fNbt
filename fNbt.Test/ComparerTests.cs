d4710ece3870b32e55f819fb0b1591db75710ff8
Matvei Stefarov <fragmer@gmail.com>
2025-07-03 23:07:02 -0700

    Implement NbtComparer and tests

---
+++ fNbt.Test/ComparerTests.cs
@@ -0,0 +1,126 @@
+﻿using NUnit.Framework;
+
+namespace fNbt.Test {
+    [TestFixture]
+    internal class ComparerTests {
+        private readonly NbtComparer comparer = NbtComparer.Instance;
+
+        [Test]
+        public void InstanceIsSingleton() {
+            Assert.That(NbtComparer.Instance, Is.SameAs(NbtComparer.Instance));
+        }
+
+        [Test]
+        public void BasicTagsEqualAndHashCode() {
+            var a = new NbtInt("foo", 123);
+            var b = new NbtInt("foo", 123);
+            var c = new NbtInt("foo", 456);    // different value
+            var d = new NbtInt("bar", 123);    // different name
+
+            Assert.IsTrue(comparer.Equals(a, b), "Same type/name/value should be equal");
+            Assert.AreEqual(comparer.GetHashCode(a), comparer.GetHashCode(b), "Equal tags must have equal hash codes");
+
+            Assert.IsFalse(comparer.Equals(a, c), "Different values => not equal");
+            Assert.IsFalse(comparer.Equals(a, d), "Different names => not equal");
+        }
+
+        [Test]
+        public void StringCaseSensitivity() {
+            var lower = new NbtString("same", "test");
+            var upper = new NbtString("same", "TEST");
+            Assert.IsFalse(comparer.Equals(lower, upper), "String comparison is case-sensitive");
+
+            // same for tag names
+            var nameLower = new NbtString("test", "same");
+            var nameUpper = new NbtString("TEST", "same");
+            Assert.IsFalse(comparer.Equals(nameLower, nameUpper), "Tag name comparison is case-sensitive");
+        }
+
+        [Test]
+        public void StringUnicode() {
+            // make sure unicode strings that are loosely comparable (in some locales) but not identical (ordinal) are not equal.
+            var uVal1 = new NbtString("same", "\u00E9"); // e with accent (precomposed)
+            var uVal2 = new NbtString("same", "\u0065\u0301"); // e + combining acute accent (decomposed)
+            Assert.IsFalse(comparer.Equals(uVal1, uVal2), "Unicode strings with different codepoints should not match");
+
+            // test the same for tag NAMES
+            var uName1 = new NbtString("\u00E9", "same");
+            var uName2 = new NbtString("\u0065\u0301", "same");
+            Assert.IsFalse(comparer.Equals(uName1, uName2), "Tag names with different codepoints should not match");
+        }
+
+        [Test]
+        public void FloatAndDoubleNaN() {
+            var f1 = new NbtFloat("f", float.NaN);
+            var f2 = new NbtFloat("f", float.NaN);
+            Assert.IsTrue(comparer.Equals(f1, f2), "NaN floats should compare equal using Equals()");
+            Assert.AreEqual(comparer.GetHashCode(f1), comparer.GetHashCode(f2), "Hash codes for NaN floats must match");
+
+            var d1 = new NbtDouble("d", double.NaN);
+            var d2 = new NbtDouble("d", double.NaN);
+            Assert.IsTrue(comparer.Equals(d1, d2), "NaN doubles should compare equal");
+            Assert.AreEqual(comparer.GetHashCode(d1), comparer.GetHashCode(d2), "Hash codes for NaN doubles must match");
+        }
+
+        [Test]
+        public void ArrayTagEquality() {
+            var b1 = new NbtByteArray("arr", new byte[] { 1, 2, 3 });
+            var b2 = new NbtByteArray("arr", new byte[] { 1, 2, 3 });
+            var b3 = new NbtByteArray("arr", new byte[] { 3, 2, 1 });
+            Assert.IsTrue(comparer.Equals(b1, b2), "Same byte arrays should be equal");
+            Assert.IsFalse(comparer.Equals(b1, b3));
+
+            var i1 = new NbtIntArray("arr", new[] { 1, -2, 3 });
+            var i2 = new NbtIntArray("arr", new[] { 1, -2, 3 });
+            Assert.IsTrue(comparer.Equals(i1, i2), "Same int arrays should be equal");
+
+            var l1 = new NbtLongArray("arr", new long[] { 10, -20, 30 });
+            var l2 = new NbtLongArray("arr", new long[] { 10, -20, 30 });
+            Assert.IsTrue(comparer.Equals(l1, l2), "Same long arrays should be equal");
+        }
+
+        [Test]
+        public void ListTagsOrderMatters() {
+            var list1 = new NbtList("l") { new NbtInt(1), new NbtInt(2), new NbtInt(3) };
+            var list2 = new NbtList("l") { new NbtInt(1), new NbtInt(2), new NbtInt(3) };
+            var list3 = new NbtList("l") { new NbtInt(3), new NbtInt(2), new NbtInt(1) };
+
+            Assert.IsTrue(comparer.Equals(list1, list2), "Same order => equal");
+            Assert.IsFalse(comparer.Equals(list1, list3), "Different order => not equal");
+        }
+
+        [Test]
+        public void CompoundTagsIgnoresOrder() {
+            var compA = new NbtCompound("c")
+            {
+                new NbtByte("a", 1),
+                new NbtByte("b", 2)
+            };
+            var compB = new NbtCompound("c")
+            {
+                new NbtByte("b", 2),
+                new NbtByte("a", 1)
+            };
+
+            Assert.IsTrue(comparer.Equals(compA, compB), "Compounds compare as sets regardless of insertion order");
+            Assert.AreEqual(comparer.GetHashCode(compA), comparer.GetHashCode(compB), "HashCode only considers count for compounds");
+        }
+
+        [Test]
+        public void ListTestEquals() {
+            // Stress-test: compare lists with every single tag type
+            var x = TestFiles.MakeListTest();
+            var y = TestFiles.MakeListTest();
+            Assert.IsTrue(comparer.Equals(x, y), "Two runs of MakeListTest should be deeply equal");
+            Assert.AreEqual(comparer.GetHashCode(x), comparer.GetHashCode(y), "And their hash codes should match");
+        }
+
+        [Test]
+        public void NullNameNotEqualEmptyName() {
+            var nullName = new NbtInt(null, 42);
+            var emptyName = new NbtInt("", 42);
+            Assert.IsFalse(comparer.Equals(nullName, emptyName), "Null name and empty name should not be equal");
+            Assert.AreNotEqual(comparer.GetHashCode(nullName), comparer.GetHashCode(emptyName), "Hash codes should not match");
+        }
+    }
+}
---
+++ fNbt/NbtComparer.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fNbt {
+    /// <summary>
+    ///   Compares NbtTag for equality by comparing their types, names, and values. Considers compound tags to be equal
+    ///   if they contain equal sets of tags. Considered list tags to be equal if their tags are equal and in the same order.
+    ///   Name comparisons are case-sensitive.
+    /// </summary>
+    public sealed class NbtComparer : IEqualityComparer<NbtTag> {
+        /// <summary> Gets a singleton instance of the NbtComparer. </summary>
+        public static NbtComparer Instance { get; } = new NbtComparer();
+
+        private NbtComparer() { }
+
+        /// <inheritdoc/>
+        public bool Equals(NbtTag x, NbtTag y) {
+            if (ReferenceEquals(x, y)) return true;
+            if (x is null || y is null) return false;
+            if (x.TagType != y.TagType) return false;
+            if (!String.Equals(x.Name, y.Name, StringComparison.Ordinal)) return false; // null names are permitted
+            return DeepEquals(x, y);
+        }
+
+        /// <inheritdoc/>
+        public int GetHashCode(NbtTag tag) {
+            if (tag is null) throw new ArgumentNullException(nameof(tag));
+
+            unchecked {
+                int hash = tag.TagType.GetHashCode() * 23;
+                if (tag.Name != null)
+                    hash ^= StringComparer.Ordinal.GetHashCode(tag.Name);
+
+                switch (tag.TagType) {
+                    case NbtTagType.ByteArray:
+                        var ba = ((NbtByteArray)tag).ByteArrayValue;
+                        hash = (hash * 23) ^ ba.Length.GetHashCode();
+                        return hash;
+
+                    case NbtTagType.IntArray:
+                        var ia = ((NbtIntArray)tag).IntArrayValue;
+                        hash = (hash * 23) ^ ia.Length.GetHashCode();
+                        return hash;
+
+                    case NbtTagType.LongArray:
+                        var la = ((NbtLongArray)tag).LongArrayValue;
+                        hash = (hash * 23) ^ la.Length.GetHashCode();
+                        return hash;
+
+                    case NbtTagType.List:
+                        var list = (NbtList)tag;
+                        hash = (hash * 23) ^ list.ListType.GetHashCode();
+                        hash = (hash * 23) ^ list.Count.GetHashCode();
+                        return hash;
+
+                    case NbtTagType.Compound:
+                        var comp = (NbtCompound)tag;
+                        hash = (hash * 23) ^ comp.Count.GetHashCode();
+                        return hash;
+
+                    default:
+                        // primitives and strings
+                        var raw = GetRawValue(tag);
+                        if (raw != null)
+                            return (hash * 23) ^ raw.GetHashCode();
+
+                        // END and unknown
+                        throw new ArgumentException("Cannot hash tags of type " + tag.TagType, nameof(tag));
+                }
+            }
+        }
+
+        // Compare detailed attributes of two given tags
+        private bool DeepEquals(NbtTag x, NbtTag y) {
+            // Assume that tags have same type and are non-null
+            switch (x.TagType) {
+                case NbtTagType.ByteArray: {
+                        var a1 = ((NbtByteArray)x).ByteArrayValue;
+                        var a2 = ((NbtByteArray)y).ByteArrayValue;
+                        if (a1.Length != a2.Length) return false;
+                        for (int i = 0; i < a1.Length; i++)
+                            if (a1[i] != a2[i]) return false;
+                        return true;
+                    }
+                case NbtTagType.IntArray: {
+                        var a1 = ((NbtIntArray)x).IntArrayValue;
+                        var a2 = ((NbtIntArray)y).IntArrayValue;
+                        if (a1.Length != a2.Length) return false;
+                        for (int i = 0; i < a1.Length; i++)
+                            if (a1[i] != a2[i]) return false;
+                        return true;
+                    }
+                case NbtTagType.LongArray: {
+                        var a1 = ((NbtLongArray)x).LongArrayValue;
+                        var a2 = ((NbtLongArray)y).LongArrayValue;
+                        if (a1.Length != a2.Length) return false;
+                        for (int i = 0; i < a1.Length; i++)
+                            if (a1[i] != a2[i]) return false;
+                        return true;
+                    }
+                case NbtTagType.Compound: {
+                        // Compounds are equal if their child-count and contents are equal
+                        var xc = (NbtCompound)x;
+                        var yc = (NbtCompound)y;
+                        if (xc.Count != yc.Count) return false;
+                        return new HashSet<NbtTag>(xc, this).SetEquals(yc);
+                    }
+                case NbtTagType.List: {
+                        // Lists are considered equal if their type, count, and contents are equal
+                        var xl = (NbtList)x;
+                        var yl = (NbtList)y;
+                        if (xl.ListType != yl.ListType || xl.Count != yl.Count) return false;
+                        for (int i = 0; i < xl.Count; i++)
+                            if (!Equals(xl[i], yl[i])) return false;
+                        return true;
+                    }
+                default: {
+                        // primitives and strings are equal if their values are equal
+                        var rawValue1 = GetRawValue(x);
+                        if (rawValue1 != null) {
+                            var rawValue2 = GetRawValue(y);
+                            return Equals(rawValue1, rawValue2);
+                        }
+                        // END and unknown
+                        throw new ArgumentException("Cannot compare tags of type " + x.TagType);
+                    }
+            }
+        }
+
+        private static object? GetRawValue(NbtTag tag) {
+            return tag.TagType switch {
+                NbtTagType.Byte => tag.ByteValue,
+                NbtTagType.Double => tag.DoubleValue,
+                NbtTagType.Float => tag.FloatValue,
+                NbtTagType.Int => tag.IntValue,
+                NbtTagType.Long => tag.LongValue,
+                NbtTagType.Short => tag.ShortValue,
+                NbtTagType.String => tag.StringValue,
+                _ => null, // End, Unknown, array, and compount tags
+            };
+        }
+    }
+}
