ebac8489e3b5ba8360646783c2b8235eefc037de
Matvei Stefarov <fragmer@gmail.com>
2026-08-30 04:14:59 -0700

    Add fixed-width int/long array benchmark rows
    Chunk-data and heightmap shapes: 4K and 1M elements, Java (swapped on
    this host) and Bedrock (native order), read and write through NbtCodec.

---
+++ fNbt.Benchmarks/NumericArrayBenchmarks.cs
@@ -0,0 +1,72 @@
+#if !FNBT_BASELINE
+using BenchmarkDotNet.Attributes;
+
+namespace fNbt.Benchmarks;
+
+// Fixed-width int/long array throughput: chunk data and heightmaps in real Java worlds.
+// Java swaps endianness on this host, Bedrock does not. NbtCodec postdates 1.1.1.
+[BenchmarkCategory(Program.BaselineIncompatible)]
+public class NumericArrayBenchmarks {
+    [ParamsSource(nameof(Flavors))]
+    public NbtFlavor Flavor = null!;
+
+    [Params(4096, 1048576)]
+    public int Length;
+
+    public static IEnumerable<NbtFlavor> Flavors => new[] { NbtFlavor.Java, NbtFlavor.Bedrock };
+
+    NbtCodec codec = null!;
+    byte[] intDoc = null!;
+    byte[] longDoc = null!;
+    NbtCompound intRoot = null!;
+    NbtCompound longRoot = null!;
+    MemoryStream sink = null!;
+
+    [GlobalSetup]
+    public void GlobalSetup() {
+        codec = NbtCodec.For(Flavor);
+        var rng = new Random(42);
+        var ints = new int[Length];
+        var longs = new long[Length];
+        for (int i = 0; i < Length; i++) {
+            ints[i] = rng.Next(int.MinValue, int.MaxValue);
+            longs[i] = (long)rng.Next() << 32 | (uint)rng.Next();
+        }
+        intRoot = new NbtCompound("r") { new NbtIntArray("a", ints) };
+        longRoot = new NbtCompound("r") { new NbtLongArray("a", longs) };
+        intDoc = codec.WriteTag(intRoot);
+        longDoc = codec.WriteTag(longRoot);
+        sink = new MemoryStream(longDoc.Length + 1024);
+    }
+
+    // Fixed-Width Array Throughput
+
+    [AverageBenchmark]
+    [Benchmark(Description = "Parse int array doc")]
+    public NbtTag ReadIntArray() {
+        return codec.ReadTag(intDoc, 0, intDoc.Length, out _);
+    }
+
+    [AverageBenchmark]
+    [Benchmark(Description = "Parse long array doc")]
+    public NbtTag ReadLongArray() {
+        return codec.ReadTag(longDoc, 0, longDoc.Length, out _);
+    }
+
+    [AverageBenchmark]
+    [Benchmark(Description = "Write int array doc")]
+    public long WriteIntArray() {
+        sink.Position = 0;
+        codec.WriteTag(intRoot, sink);
+        return sink.Position;
+    }
+
+    [AverageBenchmark]
+    [Benchmark(Description = "Write long array doc")]
+    public long WriteLongArray() {
+        sink.Position = 0;
+        codec.WriteTag(longRoot, sink);
+        return sink.Position;
+    }
+}
+#endif
